20030f4840630260698ac4f703f984058c14d921
Shubham Mishra <mishrashubham133@gmail.com>
2022-06-16 10:06:08 +0530

    updating account

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,9 +1,30 @@
 // ----------------------------------------------------------------------
+import axios from 'axios';
+const Config = require("../utils/config");
 
-const account = {
-  displayName: 'Jaydon Frankie',
-  email: 'demo@minimals.cc',
-  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
-};
+const account = {};
+
+// Fetching User Details from api
+const getProducts = async () => {
+  const config = {
+    method: 'get',
+    url: `${Config.default.BACKEND_API}/vendor/user/store`,
+    headers: {
+        "content-type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("yummittoVendorToken")}`
+    }
+  };
+  axios(config)
+    .then((res) => {
+      console.log(JSON.stringify(res.data));
+      const tempObj = res.data.categories.map((item) => item.products[0]);
+      account.name = "Yummitto Vendor";
+      account.email = "";
+      account.photoURL = "";
+    })
+    .catch((err) => {
+      console.log(JSON.stringify(err.data));
+    })
+}
 
 export default account;
