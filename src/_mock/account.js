afaa6815bde2769d0fb85f5fe524c8d3ba52b0e1
shivam-mgb <shivam.rmishra@hotmail.com>
2022-06-21 09:47:07 +0530

    accont

--- src/_mock/account.js
+++ src/_mock/account.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 
 const accountProfile = JSON.parse(localStorage.getItem('yummittoVendorProfile'));
 
 
 const account = {
-  displayName: 'Vendor Name',
-  email: 'email@gmail.com',
-  photoURL: '/static/mock-images/avatars/avatar_default.jpg',
+  displayName: accountProfile === null ? "{{VendorName}}" : accountProfile.name,
+  email: accountProfile === null ? "email@gmail.com" : accountProfile.email,
+  photoURL: accountProfile === null ? '/static/mock-images/avatars/avatar_default.jpg' : accountProfile.image,
 };
 
 export default account;
