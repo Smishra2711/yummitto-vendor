6ac24e6a9871ad407f93f6585d4d44ebfac35450
shivam-mgb <shivam.rmishra@hotmail.com>
2022-06-28 12:31:22 +0530

    inr

--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -4,7 +4,11 @@ import numeral from 'numeral';
 // ----------------------------------------------------------------------
 
 export function fCurrency(number) {
-  return numeral(number).format(Number.isInteger(number) ? '$0,0' : '$0,0.00');
+  return numeral(number).format(Number.isInteger(number) ? '0,0' : '$0,0.00');
+}
+
+export function fINRCurrency(number) {
+  return numeral(number).format(Number.isInteger(number) ? '0,0' : '₹0,0.00');
 }
 
 export function fPercent(number) {
